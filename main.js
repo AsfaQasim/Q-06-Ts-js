cf8a7803383da08e8fd5677964b4c182cd179254
Asfa Qasim <160766420+AsfaQasim@users.noreply.github.com>
2024-03-07 17:20:50 -0800

    Q 06  of Ts/js & node js of 45 assignment
    Q 06  of Ts/js & node js of 45 assignment

---
+++ main.js
@@ -0,0 +1,4 @@
+var personName = "\n\t ASIFA QASIM\t\n";
+console.log(personName);
+var stripped = personName.trim();
+console.log(stripped);
---
+++ main.ts
@@ -0,0 +1,4 @@
+let personName: string= `\n\t ASIFA QASIM\t\n`;
+console.log(personName);
+let stripped: string = personName.trim();
+console.log(stripped);
